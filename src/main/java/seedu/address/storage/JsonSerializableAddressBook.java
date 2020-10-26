2846b11dd783fd34a7a1c53be91af39f6ce813fa
iqbxl <69611716+iqbxl@users.noreply.github.com>
2020-10-26 23:40:32 +0800

    Update JsonSerializableAddressBook.java

--- src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
@@ -131,7 +131,6 @@ class JsonSerializableAddressBook {
             calorieLog.add(dailyCalorie);
         }
         addressBook.addCalorieEntries(calorieLog);
-        
         addressBook.addHeight(height);
         addressBook.addWeight(weight);
 
